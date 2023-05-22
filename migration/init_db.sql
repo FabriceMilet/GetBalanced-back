e91242109c974a9332a8f5d2b551ba4df34f08ef
FabriceMilet <milet.fab@gmail.com>
2023-05-22 14:37:45 +0200

    changement password bdd

--- migration/init_db.sql
+++ migration/init_db.sql
@@ -1,6 +1,6 @@
 -- SQLBook: Code
 -- create role for the admin db
-CREATE USER admin_gb WITH LOGIN PASSWORD 'patate';
+CREATE USER admin_gb WITH LOGIN PASSWORD 'ImoRKR4Dude23VsYxZLtGE7nE17tYn2R';
 
 
 --create database
